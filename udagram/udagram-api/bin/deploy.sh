91def785ef91e3f041d6903270925de20604fbdf
Ahmed Samir <a.s.elhaddad@gmail.com>
2023-01-17 15:48:23 +0300

    Update deploy.sh

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
-eb init 'app1api-env' –region 'us-east-1'  --platform node.js
+eb init 'app1-api' –region 'us-east-1'  --platform node.js
 eb create --sample 'App1api-env'
 eb use 'App1api-env'
-eb setenv 'AWS_DEFAULT_REGION'='us-east-1' 'AWS_User_name'='aelhadad' 'AWS_ACCESS_KEY_ID'='AKIAUTAQY5YOBXHSZTNR' 'AWS_SECRET_ACCESS_KEY'='BFLbwP4PGagaaN77F4yRw+p5VQb6Kx3ADl7WdXCl' 'AWS_Console_login_link'='https://315715284508.signin.aws.amazon.com/console'
+eb setenv  'AWS_DEFAULT_REGION'='us-east-1' 'AWS_User_name'='aelhadad' 'AWS_ACCESS_KEY_ID'='AKIAUTAQY5YOBXHSZTNR' 'AWS_SECRET_ACCESS_KEY'='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' 'AWS_Console_login_link'='https://315715284508.signin.aws.amazon.com/console'
 eb deploy || eb deploy 'App1api-env'
