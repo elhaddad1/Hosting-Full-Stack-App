cfb0003192df2f36f5e4b9eaf2bb2f1f574f2b4b
Ahmed Samir <a.s.elhaddad@gmail.com>
2023-01-18 14:25:20 +0300

    test

--- udagram/udagram-api/set_env.sh
+++ udagram/udagram-api/set_env.sh
@@ -1,13 +1,1 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_HOST="elhadad-database-postgres.cjjueppmtq7l.us-east-1.rds.amazonaws.com"
-export POSTGRES_DB="aws_store_db"
-export POSTGRES_USER="postgres"
-export POSTGRES_PASSWORD="Passw0rd12345"
-export NODE_ENV="dev"
-export PORT="5432"
-export JWT_SECRET="jllgshllWEUJHGHYJkjsfjds90"
-export AWS_REGION="us-east-1"
-export AWS_PROFILE="default"
-export AWS_BUCKET="elhadad-bucket"
-export URL="http://app1api-env.eba-2ievjpne.us-east-1.elasticbeanstalk.com/"
+eb setenv POSTGRES_HOST="elhadad-database-postgres.cjjueppmtq7l.us-east-1.rds.amazonaws.com" POSTGRES_DB="aws_store_db" POSTGRES_USER="postgres" POSTGRES_PASSWORD="Passw0rd12345" PORT="5432" JWT_SECRET="jllgshllWEUJHGHYJkjsfjds90" AWS_REGION="us-east-1" AWS_PROFILE="default" AWS_BUCKET="elhadad-bucket" URL="http://appapielh-env.eba-rwjv2y5x.us-east-1.elasticbeanstalk.com/"
